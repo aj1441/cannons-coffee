dc077bfdc8a97466e139a731726ff23e7431ebda
Aj <aj@anniecannonss-MacBook-Pro.local>
2024-06-03 11:25:49 -0700

    code before nav bar

---
+++ js/funciton.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
+
+function showHamburger() {
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active") 
+}
+
+
